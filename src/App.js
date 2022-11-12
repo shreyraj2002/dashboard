6c82b7243777a5af5cf5ff8080c079a103fc6379
Shrey Raj <shreyraj2002@gmail.com>
2022-11-12 22:26:19 +0530

    textform generation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [category, setCategory] = React.useState('');
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  }
+
+  const handleAgeChange = (e) => {
+    setAge(e.target.value);
+  }
+
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <label >
+            Name:
+          </label><br />
+          <input type="text" value={name} required onChange={(e) => { handleChange(e) }} /><br />
+          <label >
+            Type:
+          </label><br />
+          <input type="text" value={age} required onChange={(e) => { handleAgeChange(e) }} /><br />
+          <label>
+            Quantity:
+          </label><br />
+          <select name="category" value={category} onChange={(e) => { handleCategoryChange(e) }}>
+            <option id="0" >String</option>
+            <option id="1" >Numbers</option>
+            <option id="2" >Arrays</option>
+          </select>
+          <input type="submit" value="Submit" />
+        </form>
       </header>
     </div>
   );
